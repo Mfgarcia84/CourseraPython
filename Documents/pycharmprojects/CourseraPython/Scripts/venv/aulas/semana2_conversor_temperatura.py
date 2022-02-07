d965e7fc2781f2692c18cd527a10186ac6a59b77
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-02-07 15:06:01 -0300

    adicionada a funcao input na variavel temperatura Fahrenheit

--- Documents/pycharmprojects/CourseraPython/Scripts/venv/aulas/semana2_conversor_temperatura.py
+++ Documents/pycharmprojects/CourseraPython/Scripts/venv/aulas/semana2_conversor_temperatura.py
@@ -1,4 +1,4 @@
 #conversor de temperatura Fahrenheit para Celsius
-temperaturaFahrenheit = 78
+temperaturaFahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
 temperaturaCelsius = (temperaturaFahrenheit - 32) / 1.8
 print(f"{temperaturaFahrenheit} graus Fahrenheit equivale a {temperaturaCelsius:.2f} graus Celsius.")
