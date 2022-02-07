7ac8494e4e256b782a35e70d7db62c615029abe4
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-02-07 14:52:08 -0300

    conversor de temperatura

---
+++ Documents/pycharmprojects/CourseraPython/Scripts/venv/aulas/semana2_conversor_temperatura.py
@@ -0,0 +1,4 @@
+#conversor de temperatura Fahrenheit para Celsius
+temperaturaFahrenheit = 78
+temperaturaCelsius = (temperaturaFahrenheit - 32) / 1.8
+print(f"{temperaturaFahrenheit} graus Fahrenheit equivale a {temperaturaCelsius:.2f} graus Celsius.")
